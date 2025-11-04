18c444da601e96ccaef9618479810780cf37fbae
agus-darmawan <wayanagus.dr@gmail.com>
2025-11-04 09:19:09 +0700

    feat: add new documentation

--- examples/backend/src/rabbitmq/types.ts
+++ examples/backend/src/rabbitmq/types.ts
@@ -2,6 +2,7 @@ export interface VehicleLocation {
   vehicle_id: string
   latitude: number
   longitude: number
+  altitude: number
   timestamp: string
 }
 
@@ -9,6 +10,9 @@ export interface VehicleStatus {
   vehicle_id: string
   is_locked: boolean
   is_active: boolean
+  is_kill_cmd_accepted: boolean
+  is_killed: boolean
+  is_tampered: boolean
   speed: number
   heading: number
   timestamp: string
@@ -16,15 +20,21 @@ export interface VehicleStatus {
 
 export interface VehicleBattery {
   vehicle_id: string
-  battery_level: number
-  voltage: number
+  device_battery_level: number
+  device_voltage: number
+  vehicle_voltage: number
   timestamp: string
 }
 
 export interface MaintenanceReport {
   vehicle_id: string
-  issues_detected: string[]
-  recommended_maintenance: string[]
+  weight_score: 'ringan' | 'sedang' | 'berat'
+  front_tire: number
+  rear_tire: number
+  brake_pad: number
+  engine_oil: number
+  chain_or_cvt: number
+  engine: number
   timestamp: string
 }
 
@@ -33,6 +43,5 @@ export interface PerformanceReport {
   distance_travelled: number
   average_speed: number
   max_speed: number
-  fuel_efficiency: number
   timestamp: string
 }
